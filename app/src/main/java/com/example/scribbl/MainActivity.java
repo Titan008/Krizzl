2d2f0f85ca7f1c928023743569acd3a5edf03917
Titan008 <scheisekaka@gmx.de>
2020-06-16 16:25:31 +0200

    bug fix

--- app/src/main/java/com/example/scribbl/MainActivity.java
+++ app/src/main/java/com/example/scribbl/MainActivity.java
@@ -365,11 +365,7 @@ public class MainActivity extends AppCompatActivity {
                 if (snapshot != null && snapshot.exists()) {
                     Map timeData = snapshot.getData();
                     Log.e("TIME", "DATA: " + timeData.get("time"));
-                    String data = (String) timeData.get("time");
-                    if (data.equals("TIME IS OVER!")) {
-                        timer.setText("Time: Over!");
-                    }
-                    timer.setText("Time: " + data);
+                    timer.setText("Time: " + timeData.get("time"));
                 } else {
                     Log.d("TAG", "Current data: null");
                 }
